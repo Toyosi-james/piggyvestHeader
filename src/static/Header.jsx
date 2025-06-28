fe9ef380a9e0ff8c5ac18123daec7bf2f6f61de4
Toyosi-james <Jamestoyosi84@gmail.com>
2025-06-28 11:32:07 +0100

    piggyvestHeader

--- src/static/Header.jsx
+++ src/static/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Button from '../components/reusable/Button'
 import { Link } from 'react-router-dom'
 import { RxHamburgerMenu } from "react-icons/rx";
-
+import { LuChevronDown } from "react-icons/lu";
 
 
 const Header = () => {
@@ -10,12 +10,13 @@ const Header = () => {
     <div className='flex justify-between px-10 py-7 items-center mx-12 max-tablet:px-3 '>
        <section className='flex items-center gap-10 font-medium '>
        <Link to="/"> <img src='./piggyvest logo.svg' alt='' className='w-45'/></Link>
-        
-      <Link to="/Save"> <nav className='max-tablet:hidden'>Save</nav></Link>
+        <div className='flex items-center gap-1'>
+      <Link to="/Save"> <nav className='max-tablet:hidden'>Save</nav></Link> <LuChevronDown size={20} className='text-neutral-800'/></div>
        <Link to="/Invest"> <nav className='max-tablet:hidden'>Invest</nav></Link>
       <Link to="/Stories"> <nav className='max-tablet:hidden'>Stories</nav></Link>
        <Link to="/Faq"> <nav className='max-tablet:hidden'>FAQs</nav></Link>
-        <Link to="/Resources"><nav className='max-tablet:hidden'>Resources</nav></Link>
+       <div className='flex items-center gap-1'>
+        <Link to="/Resources"><nav className='max-tablet:hidden'>Resources</nav></Link><LuChevronDown size={20} className='text-neutral-800'/></div>
         
         </section>
         <section className='flex gap-4 max-tablet:hidden'>
